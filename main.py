3c60126884947e6e8b35be793eb5f3e18007b5b7
Satya Sai Nischal <85279322+coderstale@users.noreply.github.com>
2022-02-17 16:51:04 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+
+API_KEY = "e8fad847d95107128c1cdcdec98eb76f"
+burl = "http://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+
+rurl = f"{burl}?appid={API_KEY}&q={city}"
+response = requests.get(rurl)
+
+if response.status_code == 200:
+    data = response.json()
+    weather = data['weather'][0]['description']
+    temperature = round(data["main"]["temp"] - 273.15, 2)
+    print("Weather:", weather)
+    print("Temperature:", temperature, "Celsius")
+
+else:
+    print("ERROR: BAD REQ")
